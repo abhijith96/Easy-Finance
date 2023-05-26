0e29389eccc6941cf168ec2a9bdafcbd1b3f6296
abhijithka <ab903733@dal.ca>
2023-05-25 22:46:22 -0300

    register and login updated to support mysql

--- finance/application.py
+++ finance/application.py
@@ -1,13 +1,34 @@
-from cs50 import SQL
+
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from passlib.apps import custom_app_context as pwd_context
 from tempfile import mkdtemp
+import mysql.connector
+  
+
 
 from helpers import *
 
+
+def get_password():
+    path = "password.txt"
+    file1 = open(path, 'r')
+    count = 0
+    line = file1.readline()
+    return line
+
 # configure application
 app = Flask(__name__)
+
+def GetDBConnection():
+    db_handle = mysql.connector.connect(host='localhost',
+                        database='EasyFinance',
+                        user='root',
+                        password=get_password())
+    return db_handle;
+
+  
+
 
 # ensure responses aren't cached
 if app.config["DEBUG"]:
@@ -28,89 +49,54 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+
 
 @app.route("/")
 @login_required
 def index():
     s=session["user_id"]
-    irows=db.execute("SELECT * from portfolio where id=:id",id=s)
-    irows3=db.execute("SELECT count(symbol) from portfolio where id=:id", id=s)
-    n=irows3[0]['count(symbol)']
-
-    for i in range(0,n):
-        a=irows[i]["symbol"]
-        b=lookup(a)
-        if b==None:
-
-            return apology("Quote error")
-
-        sh=irows[i]["shares"]
-        c=b['price']
-        totaln=sh*c
-        db.execute("UPDATE portfolio set price=:price where symbol=:symbol and id=:id", price=c,  symbol=a, id=s)
-        db.execute("UPDATE portfolio set total=:total where symbol=:symbol and id=:id", total=totaln, symbol=a, id=s)
-
-    irows2=db.execute("SELECT * from portfolio where id=:id",id=s)
-    irows4=db.execute("SELECT sum(total) from portfolio where id=:id",id=s)
-    e=irows4[0]['sum(total)']
-    irows5=db.execute("SELECT cash from users where id=:id",id=s)
-    f=irows5[0]['cash']
-    if e:
-        g=e+f
-        return render_template("index.html", rows=irows2, gross=usd(round(g,2)), cash=usd(round(f,2)))
-    if not e:
-        return render_template("index.html", rows=irows2, cash=usd(round(f,2)))
-
-
-
-
+    return render_template("index.html")
+  
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     """Buy shares of stock."""
-    if request.method=="POST":
-        bs=request.form.get("bsymbol")
-        bs=bs.upper()
-        bsh=int(request.form.get("bshares"))
-        ba=lookup(bs)
-        if ba!= None and int(bsh)>0:
-            ncash=float(bsh)*ba['price']
-            c=session["user_id"]
-            brow= db.execute("SELECT * FROM users WHERE id = :id", id=c)
-            if brow:
-                if ncash<=brow[0]["cash"]:
-                    db.execute("INSERT INTO history(id,symbol,name,shares,price,total) VALUES(:id,:symbol,:name,:shares,:price,:total)", id=c, symbol=bs,
-                    name=bs, shares=bsh, price=ba['price'],total=ncash)
-                    brow2=db.execute("SELECT sum(shares) from history where id=:id and symbol=:symbol", id=c, symbol=bs)
-                    zz=brow2[0]["sum(shares)"]
-                    total2=float(zz)*ba['price']
-                    db.execute("INSERT OR REPLACE INTO portfolio(id,symbol,name,shares,price,total) values(:id,:symbol,:name,:shares,:price,:total)",
-                    id=c, symbol=bs, name=bs, shares=zz,price=ba['price'],total=total2)
+    if request.method == "GET":
+        return  render_template("buy.html")
 
-                    nncash=brow[0]["cash"]-ncash
-                    nrcash=usd(round(nncash,2))
-                    db.execute("UPDATE users set cash=:cash where id=:id", cash=nncash, id=c)
-                    return redirect(url_for("index"))
-                else:
-                    return apology("Not enough Funds")
-            else:
-                return apology("Log in again")
+    elif request.method=="POST":
+        # bs=request.form.get("bsymbol")
+        # bs=bs.upper()
+        # bsh=int(request.form.get("bshares"))
+        # ba=lookup(bs)
+        # if ba!= None and int(bsh)>0:
+        #     ncash=float(bsh)*ba['price']
+        #     c=session["user_id"]
+        #     brow= mysql.execute("SELECT * FROM users WHERE id = :id", id=c)
+        #     if brow:
+        #         if ncash<=brow[0]["cash"]:
+        #             mysql.execute("INSERT INTO history(id,symbol,name,shares,price,total) VALUES(:id,:symbol,:name,:shares,:price,:total)", id=c, symbol=bs,
+        #             name=bs, shares=bsh, price=ba['price'],total=ncash)
+        #             brow2=mysql.execute("SELECT sum(shares) from history where id=:id and ticker=:symbol", id=c, symbol=bs)
+        #             zz=brow2[0]["sum(shares)"]
+        #             total2=float(zz)*ba['price']
+        #             mysql.execute("INSERT OR REPLACE INTO portfolio(id,ticker,name,shares,price,total) values(:id,:symbol,:name,:shares,:price,:total)",
+        #             id=c, symbol=bs, name=bs, shares=zz,price=ba['price'],total=total2)
 
-
-
-
-
+        #             nncash=brow[0]["cash"]-ncash
+        #             nrcash=usd(round(nncash,2))
+        #             mysql.execute("UPDATE users set cash=:cash where id=:id", cash=nncash, id=c)
+        #             return redirect(url_for("index"))
+        #         else:
+        #             return apology("Not enough Funds")
+        #     else:
+        #         return apology("Log in again")
 
 
-        else:
-            return apology("Invalid stock symbol or shares")
-
-
-
-    else:
-        return render_template("buy.html")
+        # else:
+        #     return apology("Invalid stock symbol or shares
+        return apology("Buy not implemented")
 
 
 @app.route("/history")
@@ -118,8 +104,9 @@ def buy():
 def history():
     """Show history of transactions."""
     c=session["user_id"]
-    rows=db.execute("SELECT * FROM history where id=:id", id=c)
-    return render_template("history.html", rows=rows)
+    # rows=mysql.execute("SELECT * FROM history where id=:id", id=c)
+    # return render_template("history.html", rows=rows)
+    return apology("History not implemented")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -140,15 +127,22 @@ def login():
             return apology("must provide password")
 
         # query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+        db_handle = GetDBConnection()
+        cursor = db_handle.cursor()
+        rows = cursor.execute("SELECT * FROM users WHERE username = %s", (request.form.get("username"),))
+        result_tuple = cursor.fetchone()
+        print(result_tuple)
+        print(rows)
 
 
         # ensure username exists and password is correct
-        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+        if not result_tuple:
             return apology("invalid username and/or password")
+        elif not pwd_context.verify(request.form.get("password"), result_tuple[2]):
+            return apology("invalid username or/and password")
 
         # remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = result_tuple[0]
 
 
         # redirect user to home page
@@ -204,20 +198,33 @@ def register():
             return apology("must provide password")
         elif not request.form.get("confirm"):
             return apology("must confirm password")
-        a=request.form.get("password")
-        e=request.form.get("username")
-        b=request.form.get("confirm")
-        d=db.execute("SELECT * FROM users WHERE username = :username", username=e)
-
-        if a!=b:
+        
+        password=request.form.get("password")
+        username=request.form.get("username")
+        password_confirmation=request.form.get("confirm")
+    
+        if password != password_confirmation:
             return apology("Passwords do not match")
-        elif len(d)!=0:
+        
+        db_handle = GetDBConnection()
+        cursor = db_handle.cursor()
+        row_count = cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        queryed_row = cursor.fetchone()
+
+    
+        if queryed_row:
             return apology("Username exists")
         else:
-            c=pwd_context.hash(a)
-            db.execute("INSERT INTO users(username,hash) VALUES(:username, :hash)", username=request.form.get("username"),hash=c)
-            f=db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
-            session["user_id"]=f[0]["id"]
+            pwd_hash = pwd_context.hash(password)
+            print( "hash lne", len(pwd_hash))
+            cursor.execute("INSERT INTO users(username,hash) VALUES(%s, %s)", (request.form.get("username"), pwd_hash))
+            g=cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+            h = cursor.fetchone()
+            print(h)
+            if h:
+                session["user_id"]=h[0]
+                db_handle.commit()
+                db_handle.close()
             return redirect(url_for("index"))
     else:
         return render_template("register.html")
@@ -226,47 +233,47 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock."""
-    if request.method=="POST":
-        sa=request.form.get("ssymbol")
-        sb=int(request.form.get("sshares"))
-        sc=session["user_id"]
-        srows=db.execute("SELECT * from portfolio where id=:id and symbol=:symbol", id=sc, symbol=sa)
-        srows2=db.execute("SELECT cash from users where id=:id ", id=sc)
-        oldcash=srows2[0]['cash']
-
-        if srows:
-            sd=srows[0]['shares']
-
-
-            if sb <=sd:
-
-                se=lookup(sa)
-                sf=se['price']
-                sg=sf*sb
-                sh=sd-sb
-                si=sh*sf
-                newcash=oldcash+sg
-                db.execute("INSERT INTO history(id,symbol,name,shares,price,total) VALUES(:id,:symbol,:name,:shares,:price,:total)", id=sc, symbol=sa,
-                        name=sa, shares=-sb, price=sf,total=sg)
-                db.execute("update users set cash=:cash where id=:id ",cash=newcash, id=sc)
-                if sb!=sd:
-                    db.execute("update portfolio set price=:price where id=:id and symbol=:symbol",price=sf, id=sc, symbol=sa)
-                    db.execute("update portfolio set shares=:shares where id=:id and symbol=:symbol",shares=sh, id=sc, symbol=sa)
-                    db.execute("update portfolio set total=:total where id=:id and symbol=:symbol",total=si, id=sc, symbol=sa)
-
-
-                    return redirect(url_for("index"))
-                else:
-                    db.execute("delete from portfolio where id=:id and symbol=:symbol", id=sc, symbol=sa)
-                    return redirect(url_for("index"))
-
-            else:
-                return apology("You dont have enough stocks")
-        else:
-            return apology("You dont own any stocks of current company")
-
-    else:
+    if request.method == "GET":
         return render_template("sell.html")
+    elif request.method=="POST":
+        return apology("Sell not Implemented")
+        # sa=request.form.get("ssymbol")
+        # sb=int(request.form.get("sshares"))
+        # sc=session["user_id"]
+        # srows=mysql.execute("SELECT * from portfolio where id=:id and ticker=:symbol", id=sc, symbol=sa)
+        # srows2=mysql.execute("SELECT cash from users where id=:id ", id=sc)
+        # oldcash=srows2[0]['cash']
+
+        # if srows:
+        #     sd=srows[0]['shares']
+
+
+        #     if sb <=sd:
+
+        #         se=lookup(sa)
+        #         sf=se['price']
+        #         sg=sf*sb
+        #         sh=sd-sb
+        #         si=sh*sf
+        #         newcash=oldcash+sg
+        #         mysql.execute("INSERT INTO history(id,symbol,name,shares,price,total) VALUES(:id,:symbol,:name,:shares,:price,:total)", id=sc, symbol=sa,
+        #                 name=sa, shares=-sb, price=sf,total=sg)
+        #         mysql.execute("update users set cash=:cash where id=:id ",cash=newcash, id=sc)
+        #         if sb!=sd:
+        #             mysql.execute("update portfolio set price=:price where id=:id and symbol=:symbol",price=sf, id=sc, symbol=sa)
+        #             mysql.execute("update portfolio set shares=:shares where id=:id and symbol=:symbol",shares=sh, id=sc, symbol=sa)
+        #             mysql.execute("update portfolio set total=:total where id=:id and symbol=:symbol",total=si, id=sc, symbol=sa)
+
+
+        #             return redirect(url_for("index"))
+        #         else:
+        #             mysql.execute("delete from portfolio where id=:id and symbol=:symbol", id=sc, symbol=sa)
+        #             return redirect(url_for("index"))
+
+        #     else:
+        #         return apology("You dont have enough stocks")
+        # else:
+        #     return apology("You dont own any stocks of current company")
 
 
 @app.route("/addmoney", methods=["GET", "POST"])
@@ -274,14 +281,15 @@ def sell():
 def addmoney():
     if request.method=="POST":
         a=int(request.form.get("money"))
-        if a>0:
-            c=session["user_id"]
-            row=db.execute("SELECT cash from users where id=:id", id=c)
-            oldcash=row[0]['cash']
-            newcash=oldcash+a
-            db.execute("UPDATE users SET cash=:cash where id=:id", cash=newcash, id=c)
-            return render_template("addmoney2.html", am=usd(a), total=usd(newcash))
-        else:
-            return apology("Enter valid money")
+        # if a>0:
+        #     c=session["user_id"]
+        #     row=mysql.execute("SELECT cash from users where id=:id", id=c)
+        #     oldcash=row[0]['cash']
+        #     newcash=oldcash+a
+        #     mysql.execute("UPDATE users SET cash=:cash where id=:id", cash=newcash, id=c)
+        #     return render_template("addmoney2.html", am=usd(a), total=usd(newcash))
+        # else:
+        #     return apology("Enter valid money")
+        return apology("Add Money not Implemented")
     else:
         return render_template("addmoney.html")
